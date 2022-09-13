9caa0c2934dee9d5871fa1168c0d4147033ad265
Facundo Rodriguez <facundo-grodriguez@hotmail.com.ar>
2022-09-13 10:40:57 -0300

    interactuando con html

--- js/sales.js
+++ js/sales.js
@@ -1,10 +1,10 @@
 // LLAMADAS AL HTML
-const search = document.querySelector("#search")
-const filter = document.querySelector("#filter")
-const lower = document.querySelector("#lowerPrice")
-const higher = document.querySelector("#higherPrice")
-const addingNewProducts = document.querySelector("#addingProducts")
-const container = document.querySelector("#container")
+const filter = document.querySelector("#filter")//Filtro
+const everyBody = document.querySelector("#everybody")//Todos los productos
+const lower = document.querySelector("#lowerPrice")//Menor precio
+const higher = document.querySelector("#higherPrice")//Mayor precio
+const addingNewProducts = document.querySelector("#addingProducts")//Cargando productos
+const container = document.querySelector("#container")//Contenedor de cards
 
 // VARIABLES NECESARIAS 
 
@@ -12,7 +12,7 @@ const container = document.querySelector("#container")
 
 // CLASE CONSTRUCTORA   
 class Product {
-    constructor(brand, name, image, price, quantity) {
+    constructor(brand, name, image, price) {
         this.brand = brand
         this.name = name
         this.image = image
@@ -36,16 +36,15 @@ const cart = []
 
 // LISTA DE PRODUCTOS 
 function list() {
-    
-    products.push(new Product("XIAOMI", "REDMI 9A", "./assets/images/redmi-9A.jpg" , 145))
-    products.push(new Product("XIAOMI", "REDMI 9C", "./assets/images/redmi-9C.jpg", 175))
-    products.push(new Product("XIAOMI", "REDMI 9T","./assets/images/redmi-9t.jpg", 210))
-    products.push(new Product("XIAOMI", "NOTE 9 PRO","./assets/images/redmiNote9Pro.jpg", 290))
-    products.push(new Product("XIAOMI", "REDMI 10","./assets/images/redmi-10.png", 215))
-    products.push(new Product("XIAOMI", "REDMI 10A","./assets/images/redmi-10a.jpg", 180))
-    
-}
 
+    products.push(new Product("XIAOMI", "REDMI 9A", "./assets/images/redmi-9A.jpg", 145))
+    products.push(new Product("XIAOMI", "REDMI 9C", "./assets/images/redmi-9C.jpg", 175))
+    products.push(new Product("XIAOMI", "REDMI 9T", "./assets/images/redmi-9t.jpg", 210))
+    products.push(new Product("XIAOMI", "NOTE 9 PRO", "./assets/images/redmiNote9Pro.jpg", 290))
+    products.push(new Product("XIAOMI", "REDMI 10", "./assets/images/redmi-10.png", 215))
+    products.push(new Product("XIAOMI", "REDMI 10A", "./assets/images/redmi-10a.jpg", 180))
+
+}
 
 
 // AGREGADO DE PRODUCTOS
@@ -59,53 +58,28 @@ const addingProducts = () => {
     let newProduct = new Product(brand, name, image, price)
 
     products.push(newProduct)
-
 }
-//BOTON
-addingNewProducts.addEventListener("click", () => { addingProducts()} ) 
+
+
 
 // CARGANDO PRODUCTOS
-const loadProducts = (products) =>{
-    for (const product of products){
-        
-        let div = document.createElement ("div")
-        div.setAttribute("class", "card text-center ")
-        
-    div.innerHTML = ` 
-                        <h2>${product.name}</h2>
-                        <h4>${product.brand}</h4>
-                        <img  class="image" src="${product.image}" onerror="this.src='./assets/images/remodelacion.jpg'" alt="Hubo un error">
-                        <p class="price"><strong>${product.price} USD</strong></p>
-                        <button class="button btn" id="cart" >Agregar al carro</button>
-                    `
-                    container.appendChild(div)
+let div = ""
+const loadProducts = (products) => {
+    for (const product of products) {
+        div = document.createElement("div")
+        div.setAttribute("class", "card text-center col-xl-3 col-md-4 col-8  m-3")
+
+        div.innerHTML = ` 
+        <h2>${product.name}</h2>
+        <h4>${product.brand}</h4>
+        <img  class="image" src="${product.image}" onerror="this.src='./assets/images/remodelacion.jpg'" alt="Hubo un error">
+        <p class="price"><strong>${product.price} USD</strong></p>
+        <button class="button btn " id="cart" >Agregar al carro</button>
+        `
+        container.appendChild(div)
     }
-    
-    
+
 }
-list()
-loadProducts(products)
-
-
-
-//PARA EMPEZAR Y LLEGADO AL CASO SEGUIR AGREGANDO PRODUCTOS
-function runProducts () {
-
-    let loadProduct = confirm("¿Quiere cargar un nuevo producto?")
-    
-        if (loadProduct !== true){
-            loadProducts(products)
-            
-        }
-    
-    while (loadProduct === true) {
-        addingProducts()
-        loadProducts(products)
-        loadProduct = confirm("¿Quiere cargar otro producto?")
-    }
-    
-}
-
 
 
 //PARA ORDENAR LOS PRECIOS
@@ -119,10 +93,7 @@ function lowerPrice() {
         }
         return 0
     })
-    
-} 
-//BOTON
-lower.addEventListener("click", () => { lowerPrice()} ) 
+}
 
 
 function higherPrice() {
@@ -136,21 +107,29 @@ function higherPrice() {
         return 0
     })
 }
-//BOTON
-higher.addEventListener("click", () => { higherPrice()} ) 
 
-
-//BUSQUEDA EN DETALLE
-function filtering() {
-    let parameter = prompt("Ingrese el nombre especifico de lo que busca: ").toUpperCase()
-    let result = products.filter((product) => product.name.includes(parameter))
-    console.table(result)
+function newList() {
+    if (higherPrice) {
+        products.value = ""
+        higherPrice()
+    } else {
+        products.value = ""
+        lowerPrice()
+    }
 }
 
 
 //MAS DE UN RESULTADO
 function searching() {
-    let nameProd = filter.value.toUpperCase()
+    let search = []
+    let parameter = filter.value.toUpperCase()
+    let result = products.filter((product) => product.name.includes(parameter))
+    //document.write(search.push(result))
+}
+
+//BUSQUEDA EN DETALLE
+function filtering() {
+    let nameProd = prompt("Ingrese el nombre especifico de lo que busca: ").toUpperCase()
     let result = products.find((product) => product.name.includes(nameProd))
     if (result !== undefined) {
         console.table(result)
@@ -158,5 +137,27 @@ function searching() {
         console.warn("No se encontró el producto buscado.")
     }
 }
-//BOTON
-search.addEventListener("click", () => { searching()} ) 
+
+
+//BOTONES
+
+//MAYOR PRECIO
+higher.addEventListener("click", () => {
+    if (higherPrice) {
+        container.parentNode.removeChild(div)
+        higherPrice()
+    }
+})
+//MENOR PRECIO
+lower.addEventListener("click", () => {
+    debugger
+    if (lowerPrice) {
+        if(div.parentNode){
+        div.parentNode.removeChild(div)
+        lowerPrice()
+    }}
+})
+//AGREGADO DE PRODUCTOS
+addingNewProducts.addEventListener("click", () => addingProducts())
+//MUESTRA LOS PRODUCTOS
+everyBody.addEventListener("click", () => loadProducts(products), list())
